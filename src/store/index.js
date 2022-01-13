767666cd46ac1d3eafef87bd4cd355445aa71d38
auoHan <710543268@qq.com>
2022-01-13 22:32:36 +0800

    创建store的文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import note from '@/store/modules/note'
+import notebook from '@/store/modules/notebook'
+import trash from '@/store/modules/trash'
+import user from '@/store/modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    note,
+    notebook,
+    trash,
+    user
   }
 })
