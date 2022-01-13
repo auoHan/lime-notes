e9da4cdaf2d4221a48d7ae338d04591c7cfc433f
auoHan <710543268@qq.com>
2022-01-14 05:29:20 +0800

    解决时间不显示，排序没效果的问题

--- src/apis/notes.js
+++ src/apis/notes.js
@@ -17,9 +17,7 @@ export default {
             note.createdAtFriendly = friendlyDate(note.createdAt)
             note.updatedAtFriendly = friendlyDate(note.updatedAt)
             return note
-          }).sort((note1, note2) => {
-            return note1.updatedAt < note2.updatedAt
-          })
+          }).sort((note1, note2) => note1.updatedAt < note2.updatedAt ? 1 : -1)
           resolve(res)
         }).catch(err => {
         reject(err)
