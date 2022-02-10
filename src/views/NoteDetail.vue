7474ea8d1b75cdb9f6a9ff50ba8b11b656cdc146
auoHan <710543268@qq.com>
2022-02-10 15:59:34 +0800

    溢出BUG修改

--- src/views/NoteDetail.vue
+++ src/views/NoteDetail.vue
@@ -190,6 +190,7 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
+    overflow: auto;
     padding: 20px;
   }
 
--- src/views/TrashDetail.vue
+++ src/views/TrashDetail.vue
@@ -235,6 +235,7 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
+    overflow: auto;
     padding: 20px;
   }
 
